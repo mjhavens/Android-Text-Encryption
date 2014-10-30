42d01ce779d8d77b039ac24a4669a976b3ee65d6
MJ Havens <me@mjhavens.com>
2014-10-30 16:39:06 -0500

    Initial commit.

---
+++ src/com/mjhavens/textencryption/EncryptionActivity.java
@@ -0,0 +1,169 @@
+/*
+ * @author: MJ Havens
+ * 
+ * This class is mainly used for encrypting purposes.
+ * It encryptes, decryptes, encodes, and decodes plaintext and ciphertext.
+ */
+package com.mjhavens.textencryption;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import android.app.Activity;
+import android.util.Log;
+
+public class EncryptionActivity extends Activity
+{
+	private byte[]	encryptedText	= null;
+    private String base64CipherText = null;
+    private byte[] textBytes = null;
+    private byte[] secretKeyBytes = null;
+    private String strdecryptedText;
+    private byte[] secretKeyInBytes = null;
+    
+    /**
+     * The default constructor sets all variables to null.
+     */
+	public EncryptionActivity()
+	{
+		encryptedText	= null;
+	    base64CipherText = null;
+	    textBytes = null;
+	    secretKeyBytes = null;
+	    strdecryptedText = null;
+	}
+	
+	/**
+	 * 
+	 * @param secretKey the secret key for the encryption.
+	 * 
+	 * This method generates a key based on the secret key 
+	 * and a initialization vector using AES.
+	 */
+	public void prepareKeyForEncryption(String secretKey) throws NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, IOException
+	{
+		
+		secretKeyBytes = secretKey.getBytes("UTF-8");
+
+		// This generates cryptographically secure random numbers using AES in ECB mode.
+		SecureRandom sR = SecureRandom.getInstance("SHA1PRNG");
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+		sR.setSeed(secretKeyBytes);
+		Log.d("App", "Secret key in bytes: " + secretKeyBytes);
+		Log.d("App", "SecureRandom string: " + sR.toString());
+
+		// Initialize the 128 bit key generator using the secret key and AES.
+		keyGen.init(128, sR);
+
+		// Generate the actual 128 bit key based on the secret key and AES.
+		SecretKey skey = keyGen.generateKey();
+
+		// The bytes for the secret key that was generated.
+		secretKeyBytes = skey.getEncoded();
+
+	}
+
+	/**
+	 * 
+	 * @param encryptedKey the key that was encrypted based on the prepareKeyForEncryption() method.
+	 * @param textToEncrypt The plaintext that needs to be encrypted to ciphertext.
+	 *
+	 * Uses AES/ECB to encrypt the plaintext based on the encrypted secret key.
+	 */
+	public void encryptWithAES(byte[] encryptedKey, String textToEncrypt)
+			throws IllegalBlockSizeException, BadPaddingException,
+			NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, UnsupportedEncodingException
+	{
+		textBytes = textToEncrypt.getBytes("UTF-8");
+		Log.d("App", "encrypted key: " + encryptedKey);
+
+		SecretKeySpec sKey = new SecretKeySpec(encryptedKey, "AES");
+		Cipher c = Cipher.getInstance("AES/ECB/PKCS7Padding");
+		c.init(Cipher.ENCRYPT_MODE, sKey);
+		Log.d("App", "encrypted skey: " + sKey);
+		encryptedText = c.doFinal(textBytes);
+		Log.d("App", "Encrypted bytes: " + encryptedText);
+		encodeToBase64(encryptedText);
+		
+
+	}
+
+	/**
+	 * @param encryptedText The output of the encrypted text in bytes.
+	 * 
+	 * Uses the bytes of the ciphertext to encode it to base 64.
+	 */
+	private void encodeToBase64(byte[] encryptedText)
+	{
+		
+		base64CipherText = Base64.encodeBytes(encryptedText);	
+		Log.d("App", "Base 64 encrypted text: " + base64CipherText);
+	}
+	
+	/**
+	 * 
+	 * @param base64CipherText The cipher text encoded with base 64.
+	 * @return The bytes of the ciphertext.
+	 */
+	private byte[] decodeFromBase64(String base64CipherText) throws IOException
+	{
+		byte[] cipherText = Base64.decode(base64CipherText);
+		return cipherText;
+		
+	}
+	
+	/**
+	 * 
+	 * @param encryptedKey The AES encrypted key.
+	 * @param base64CipherText The base 64 encoded cipher text.
+	 * 
+	 * Decrypts the AES base 64 encoded ciphertext into plaintext.
+	 */
+	public void decryptAES(byte[] encryptedKey, String base64CipherText)
+			throws NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException
+	{
+
+		SecretKeySpec sKey = new SecretKeySpec(encryptedKey, "AES");
+		Cipher c = Cipher.getInstance("AES");
+		c.init(Cipher.DECRYPT_MODE, sKey);
+		strdecryptedText = new String(c.doFinal(decodeFromBase64(base64CipherText)));
+		Log.d("App", "Plaintext: " + strdecryptedText);
+	}
+	
+	/**
+	 * @return The plaintext.
+	 */
+	public String getDecryptedText()
+	{
+		return strdecryptedText;
+	}
+	
+	/**
+	 * @return The secret key in bytes.
+	 */
+	public byte[] getSecretKeyBytes()
+	{
+		return secretKeyBytes;
+	}
+
+	/**
+	 * @return The ciphertext encoded in base 64.
+	 */
+	public String getBase64CipherText()
+	{
+		return base64CipherText;
+	}
+}
---
+++ src/com/mjhavens/textencryption/MainActivity.java
@@ -0,0 +1,140 @@
+/*
+ * @author: MJ Havens
+ * 
+ * This is the main activity that gets called when the app starts. 
+ * It is used to control input and output on the GUI while interacting with EncryptionActivity.
+ */
+
+package com.mjhavens.textencryption;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.aqhrazrkjsqtqqiqm.AdController;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+	private EditText			textInput;
+	private EditText			keyInput;
+	private TextView			outputText;
+	private TextView			cryptedText;
+	private Button				encrypt;
+	private Button				decrypt;
+	private String				inputText	= null;
+	private String				keyText		= null;
+	private EncryptionActivity	encrypter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_encryption);
+
+		// Declare View IDs.
+		encrypter = new EncryptionActivity();
+		textInput = (EditText) findViewById(R.id.textInput);
+		encrypt = (Button) findViewById(R.id.btnEncrypt);
+		decrypt = (Button) findViewById(R.id.btnDecrypt);
+		keyInput = (EditText) findViewById(R.id.keyInput);
+		outputText = (TextView) findViewById(R.id.outputText);
+		cryptedText = (TextView) findViewById(R.id.cryptedText);
+
+		
+		// Sets the listener each time the encrypt button is hit.
+		encrypt.setOnClickListener(new View.OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				Log.d("App", "Clicked encrypt");
+				inputText = textInput.getText().toString();
+				keyText = keyInput.getText().toString();
+				try
+				{
+					if (inputText.isEmpty() || keyText.isEmpty())
+					{
+						cryptedText.setText("Error:");
+						outputText
+								.setText("Please set your plaintext and key before encrypting.");
+					}
+					else
+					{
+						cryptedText.setText("AES/Base 64 Cipher Text:");
+						encrypter.prepareKeyForEncryption(keyText);
+						encrypter.encryptWithAES(encrypter.getSecretKeyBytes(),
+								inputText);
+						outputText.setText(encrypter.getBase64CipherText());
+					}
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// Sets the listener each time the decrypt button is hit.
+		decrypt.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Log.d("App", "Click decrypt");
+
+				try
+				{
+					if (encrypter.getBase64CipherText() == null)
+					{
+						cryptedText.setText("Error: ");
+						outputText.setText("There is nothing to decrypt!");
+					}
+					else
+					{
+						cryptedText.setText("Plaintext:");
+						encrypter.decryptAES(encrypter.getSecretKeyBytes(),
+								encrypter.getBase64CipherText());
+						outputText.setText(encrypter.getDecryptedText());
+					}
+				}
+				catch (Exception e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.encryption, menu);
+		return true;
+	}
+
+}
